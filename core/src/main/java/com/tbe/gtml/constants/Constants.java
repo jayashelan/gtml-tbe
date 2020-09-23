d531e128979287d0967e667b26b3d0c34c259364
jayashelan <jayashelan.boobalakrishnan@gmail.com>
2020-09-23 17:48:31 +0800

    Initial Commit

---
+++ core/src/main/java/com/tbe/gtml/constants/Constants.java
@@ -0,0 +1,19 @@
+package com.tbe.gtml.constants;
+
+
+public final class Constants {
+
+    private Constants() {
+
+    }
+
+    /** key value separator used in kafka url builder **/
+    public static final String KEY_VALUE_SEPARATOR = "=";
+    /** query parameter starting point in kafka url builder **/
+    public static final String URL_QUERY_PARAM = "?";
+    /** query parameters separator used in kafka url builder **/
+    public static final String URL_QUERY_PARAMS_DELIMITER = "&";
+
+
+
+}
